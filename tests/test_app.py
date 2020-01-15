c1e4bd60e0ed2a798a837582b03496f68ea7f83b
Aaron Kau <aaron.kau@rutgers.edu>
2020-01-15 14:36:27 -0500

    add failing build

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import add_numbers, subtract_numbers
+from src.app import add_numbers, broken_add_numbers, subtract_numbers
 
 def setup():
     pass
@@ -11,3 +11,7 @@ def test_add_numbers():
 
 def test_subtract_numbers():
 	assert subtract_numbers(7,3) == 4
+
+def test_broken_add_numbers():
+	# Expected to fail
+	assert broken_add_numbers(7, 12) == 19
