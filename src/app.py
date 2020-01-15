b2693fc5603e567354a88a5329c8fc7057b37d8c
Aaron Kau <aaron.kau@rutgers.edu>
2020-01-15 14:39:08 -0500

    Spacing fix

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ def add_numbers(a, b):
     return a + b
 
 def subtract_numbers(a, b):
-	return a - b
+    return a - b
 
 def broken_add_numbers(a, b):
-	return a + b + 1
+    return a + b + 1
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,8 +10,8 @@ def test_add_numbers():
     assert add_numbers(3,4) == 7
 
 def test_subtract_numbers():
-	assert subtract_numbers(7,3) == 4
+    assert subtract_numbers(7,3) == 4
 
 def test_broken_add_numbers():
-	# Expected to fail
-	assert broken_add_numbers(7, 12) != 19
+    # Expected to fail
+    assert broken_add_numbers(7, 12) != 19
